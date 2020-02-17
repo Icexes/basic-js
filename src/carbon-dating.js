0d14788d1be21fcba32bf90065daa9955aed65ad
Denis <suboch.denny@gmail.com>
2020-02-17 11:19:47 +0300

    feat: implement carbon-dating func

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,9 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity = false ) {
+  let floatSampleActivity = parseFloat(sampleActivity)
+  return typeof sampleActivity =='string' ? 
+  (isNaN(floatSampleActivity) || floatSampleActivity<=0 || floatSampleActivity>15  ? false :   Math.ceil(Math.log(MODERN_ACTIVITY/floatSampleActivity)*HALF_LIFE_PERIOD/0.693))
+   : false;
 };
