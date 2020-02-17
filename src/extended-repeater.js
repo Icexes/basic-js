99fa4ccdc50af07043b174c398012bb588766aec
Denis <suboch.denny@gmail.com>
2020-02-17 16:41:08 +0300

    feat: implement extended-repeater func

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,21 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+     
+    if (options.addition === undefined) options.addition = "" ;
+    if (!options.repeatTimes) options.repeatTimes = 1 ;
+    if (!options.additionRepeatTimes) options.additionRepeatTimes = 1 ;
+    if (!options.separator) options.separator = '+' ;
+    if(!options.additionSeparator) options.additionSeparator = '|' ; 
+   
+    let strWithAddition = str;
+    for (let j =0; j<options.additionRepeatTimes;j++) {
+        j!== options.additionRepeatTimes-1 ? strWithAddition+= options.addition + options.additionSeparator : strWithAddition+= options.addition
+    }
+
+    let finalStr="";
+    for (let j =0; j<options.repeatTimes;j++) {
+        j!== options.repeatTimes-1 ? finalStr= finalStr + strWithAddition + options.separator  : finalStr+=strWithAddition
+    }
+    return finalStr
+
 };
   
