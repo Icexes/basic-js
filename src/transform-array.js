424bb495387336647f50c6cf1c102df58bd441b0
Denis <suboch.denny@gmail.com>
2020-02-21 09:51:25 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,32 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports =
+    function transform(arr) {
+        if (!Array.isArray(arr)) throw Error;
+        let finalArr = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == '--double-next') {
+                if (i + 1 == arr.length) break;
+                finalArr.push(arr[i + 1]);
+                continue;
+            }
+            else if (arr[i] == '--discard-next') {
+                if (i + 1 == arr.length) break;
+                i++;
+                continue;
+            }
+            else if (arr[i] == '--double-prev') {
+                if (i - 1 < 0) continue;
+                finalArr.push(arr[i - 1]);
+                continue;
+            }
+            else if (arr[i] == '--discard-prev') {
+                if (i - 1 < 0) continue;
+                finalArr.pop();
+                continue;
+            }
+            else finalArr.push(arr[i]);
+        }
+
+        return finalArr;
+    };
+
+
