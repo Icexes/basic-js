51736299c3cdbf53035c1d0279eef43d2099b0de
Denis <suboch.denny@gmail.com>
2020-02-28 10:43:32 +0300

    feat: add solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,30 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+
+        return arr.reduce((accum, curr) => {
+            let counter = 1;
+            if (Array.isArray(curr)) {
+                counter += this.calculateDepth(curr)
+            }
+            if (counter > accum) {
+                accum = counter
+            }
+            return accum;
+        }, 1)
     }
-};
+}
+  //  console.log(calculateDepth([1,2,3]))
+//alert(calculateDepth([1,[[1,2,3,4],2],2]))
+//alert(calculateDepth([[]]))
+//alert(calculateDepth([1, 2, 3, 4, 5]))
+//alert(calculateDepth([[[]]]))
+//alert(calculateDepth([1, 2, 3, [1], 4, 5, [1]]))
+
+
+
+// alert(calculateDepth([1, 2, 3, [8, [2]], 4, 5, []]));
+// alert(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]), 4);
+//  alert(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 5);
+//  alert(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 15);
+// alert(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 25);
+//  alert(calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 31);
