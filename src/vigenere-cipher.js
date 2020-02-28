2a2b36484b548f8d2d688b3d3b4f01ca5287ab19
Denis <suboch.denny@gmail.com>
2020-02-28 14:37:52 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,65 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(type = true) {
+        this.modification = type;
+    }
+    vignereSquare = [
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        "BCDEFGHIJKLMNOPQRSTUVWXYZA",
+        "CDEFGHIJKLMNOPQRSTUVWXYZAB",
+        "DEFGHIJKLMNOPQRSTUVWXYZABC",
+        "EFGHIJKLMNOPQRSTUVWXYZABCD",
+        "FGHIJKLMNOPQRSTUVWXYZABCDE",
+        "GHIJKLMNOPQRSTUVWXYZABCDEF",
+        "HIJKLMNOPQRSTUVWXYZABCDEFG",
+        "IJKLMNOPQRSTUVWXYZABCDEFGH",
+        "JKLMNOPQRSTUVWXYZABCDEFGHI",
+        "KLMNOPQRSTUVWXYZABCDEFGHIJ",
+        "LMNOPQRSTUVWXYZABCDEFGHIJK",
+        "MNOPQRSTUVWXYZABCDEFGHIJKL",
+        "NOPQRSTUVWXYZABCDEFGHIJKLM",
+        "OPQRSTUVWXYZABCDEFGHIJKLMN",
+        "PQRSTUVWXYZABCDEFGHIJKLMNO",
+        "QRSTUVWXYZABCDEFGHIJKLMNOP",
+        "RSTUVWXYZABCDEFGHIJKLMNOPQ",
+        "STUVWXYZABCDEFGHIJKLMNOPQR",
+        "TUVWXYZABCDEFGHIJKLMNOPQRS",
+        "UVWXYZABCDEFGHIJKLMNOPQRST",
+        "VWXYZABCDEFGHIJKLMNOPQRSTU",
+        "WXYZABCDEFGHIJKLMNOPQRSTUV",
+        "XYZABCDEFGHIJKLMNOPQRSTUVW",
+        "YZABCDEFGHIJKLMNOPQRSTUVWX",
+        "ZABCDEFGHIJKLMNOPQRSTUVWXY",
+    ]
+    encrypt(message, key) {
+        if (!message || !key) throw Error;
+        let result = [];
+        let key1 = key.repeat(Math.ceil(message.length / key.length)).toUpperCase()
+        for (let i = 0, j = 0; i < message.length; i++ , j++) {
+            if (/[a-z]/i.test(message.charAt(i))) {
+                result.push(this.vignereSquare[key1.charAt(j).charCodeAt() - 65][message.toUpperCase().charAt(i).charCodeAt() - 65])
+            }
+            else {
+                result.push(message.charAt(i))
+                j--;
+            }
+        }
+        return this.modification ? result.join("") : result.reverse().join("");
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(message, key) {
+        if (!message || !key) throw Error;
+        let result = [];
+        let key1 = key.repeat(Math.ceil(message.length / key.length)).toUpperCase()
+        for (let i = 0, j = 0; i < message.length; i++ , j++) {
+            if (/[a-z]/i.test(message.charAt(i))) {
+                result.push(this.vignereSquare[0][this.vignereSquare[key1.charAt(j).charCodeAt() - 65].indexOf(message.charAt(i))])
+            }
+            else {
+                result.push(message.charAt(i))
+                j--;
+            }
+        }
+        return this.modification ? result.join("") : result.reverse().join("");
     }
 }
-
 module.exports = VigenereCipheringMachine;
